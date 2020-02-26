64bc6667139e803634ef48bf66f8a83f1bb51cee
Ayomikun Orogun <orogun@amazon.com>
2020-02-26 15:25:16 -0600

    Create and set up bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 import click
 
 session = boto3.Session(profile_name='pythonAutomation')
@@ -10,11 +11,13 @@ def cli():
     "Webotron deploys websites to AWS"
     pass
 
+
 @cli.command('list-buckets')
 def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket-name')
@@ -24,7 +27,50 @@ def list_bucket_objects(bucket_name):
         print(obj)
 
 
+@cli.command('setup-bucket')
+@click.argument('bucket-name')
+def setup_bucket(bucket_name):
+    "Create and configure an S3 bucket"
+    s3_bucket = None
+
+    try:
+        s3_bucket = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
+                                 'LocationConstraint': session.region_name})
+    
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket_name)
+        else:
+            raise e    
+
+    policy = """
+    {
+    "Version":"2012-10-17",
+    "Statement":[{
+    "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+    "Principal": "*",
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::%s/*"
+        ]
+        }
+    ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={'ErrorDocument': {
+        'Key': 'error.html'
+    },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+    }})
+
+    return
 
 if __name__ == '__main__':
     cli()
-    
